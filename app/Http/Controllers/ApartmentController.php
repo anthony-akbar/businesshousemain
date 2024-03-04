637258d24bdfd1aec0f96102b6f8bc17a070ff2f
mamadjanovakbar@gmail.com <AkbarGithub8522>
2024-03-04 16:48:50 +0600

    Renamed and used Request

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApartmentRequest;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -20,9 +21,9 @@ class ApartmentController extends Controller
         return view('apartment.create');
     }
 
-    public function store(Request $request)
+    public function store(ApartmentRequest $request)
     {
-        $data = $request->all();
+        $data = $request->validated();
         $image = Storage::put('/images', $data['image']);
         unset($data['image']);
         $data['image'] = $image;
--- app/Http/Requests/ApartmentRequest.php
+++ app/Http/Requests/ApartmentRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class Appartment extends FormRequest
+class ApartmentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
